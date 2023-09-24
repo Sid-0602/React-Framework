53efa035bd31f9dd3c40848d18ce8c7d2affe7c9
Siddhant J <sidbnjadhav@gmail.com>
2023-09-24 15:02:51 +0530

    components and props

--- react-basics/src/App.js
+++ react-basics/src/App.js
@@ -1,7 +1,41 @@
 import './App.css';
 
 function App() {
-  return <div className="App">Hello World!</div>;
+
+  //this is how component is called into A
+  return <div className="App">
+    <User name="Siddhant" age={21} email="sid@gmail.com"/>
+    <User /> 
+  </div>;
 }
+
+//just a JS function. 
+// const GetName = () =>{
+//   return "Siddhant"
+// }
+
+//component starts with capital letter always. 
+const User = (props) =>{ 
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h2>{props.age}</h2>
+      <h2>{props.email}</h2>
+    </div>
+  )
+}
+
+//props: This is used to pass data from one component to another.
+// props becomes an object with the following structure: 
+/*
+  props = {
+    name=" ",
+    age=" ",
+    email= " "
+  }
+
+*/
+
+
 
 export default App;
