963689b33e6114a3a06c7d22c613647d4bf69fe7
Siddhant J <sidbnjadhav@gmail.com>
2023-10-01 10:29:25 +0530

    UseSatate Hook in react

--- react-basics/src/App.js
+++ react-basics/src/App.js
@@ -1,5 +1,6 @@
 import styles from "./App.module.css";
 import { Users } from "./components/Users.js";
+import { useState } from "react";
 
 
 function App() {
@@ -7,6 +8,30 @@ function App() {
     {name: "Elon", age:46},
     {name: "Jeff", age:50}
   ];
+
+  //useStates in React: 
+  //let person_age = 18;
+  let [person_age,setAge] = useState(0); //this is useState function.
+  const incrementAge = () =>{
+    setAge(person_age + 1);
+  }
+  const decrementAge = () =>{
+    setAge(person_age-1);
+  }
+
+  const setZeroAge = () =>{
+    setAge(0);
+  }
+  //text component: 
+  let [inputVal, setInputVal] = useState("");
+  const handleInputVal = (event)=>{
+    console.log(event.target.value); //this prints every change in text box.
+    setInputVal(event.target.value);
+  }
+
+  //hide/show text: 
+  let [showText, setForm] = useState(true);
+
   //this is how component is called into main App function.
   return (<div className={styles.App}>
     <User name="Siddhant" age={21} email="sid@gmail.com"/>
@@ -24,6 +49,25 @@ function App() {
           <Users name = {user.name} age={user.age}/>
         )
       })}
+
+    <h1>|| Click this button to increment/decrease/set to 0 ||</h1>
+    <div>
+      <h2>{person_age}</h2>
+      <button onClick={incrementAge}>Increase</button>
+      <button onClick={decrementAge}>Decrease</button>
+      <button onClick={setZeroAge}>=ZERO</button>
+    </div>
+    <div>
+      <input type="text" onChange={handleInputVal} /> {inputVal}
+    </div>
+
+    <div>
+      <h2>Click to Change Visibility</h2>
+      <button onClick={()=>{
+        setForm(!showText);
+      }}>Click</button>
+      {showText === true && <h3>Hello there!</h3>}
+    </div>
   </div> )
 }
 
@@ -99,6 +143,11 @@ const NamesList = ()=>{
   );
 }
 
+//Use States: 
+/*
+The useState() is a Hook that allows you to have state variables in functional components .
+so basically useState is the ability to encapsulate local state in a functional component. 
+*/
 
 
 export default App;
