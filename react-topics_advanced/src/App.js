60631ec5bf525fa7ad5d3140365caede13fe18fd
Siddhant J <sidbnjadhav@gmail.com>
2023-10-04 12:00:10 +0530

    Fetching data with React Query

--- react-topics_advanced/src/App.js
+++ react-topics_advanced/src/App.js
@@ -5,13 +5,20 @@ import { Profile } from './pages/Profile';
 import { Contact } from './pages/Contact';
 import { Navbar } from './pages/Navbar';
 import { useState, createContext } from 'react';
+import { Query, QueryClient,QueryClientProvider } from '@tanstack/react-query';
 
 //creating and using context is used to eliminate usage of props to parse data into components.
 export const AppContext = createContext();
 function App() {
   const [username, setUsername] = useState("Sid Jadhav"); //this is passed as props to profile->changeProfile.
+  const client = new QueryClient({defaultOptions:{
+    queries:{
+      refetchOnWindowFocus: false, //this will not refresh data on tab switch.
+    }
+  }}); //client for react-query.
   return (
     <AppContext.Provider value={{username,setUsername}} >
+    <QueryClientProvider client={client}>
     <Router>
       <Navbar />
       <Routes>
@@ -21,6 +28,7 @@ function App() {
         <Route path='*' element={<h1>404! Page not found!!</h1>} />
       </Routes>
     </Router>
+    </QueryClientProvider>
     </AppContext.Provider>
   );
 }
--- react-topics_advanced/src/pages/Home.js
+++ react-topics_advanced/src/pages/Home.js
@@ -1,7 +1,24 @@
 import React from "react";
 import { useContext } from "react";
 import { AppContext } from "../App";
+import { useQuery } from "@tanstack/react-query"; 
+import Axios from "axios";
+
 export const Home = (props) =>{
     const {username} = useContext(AppContext);
-    return <h1 className="App"> This is home page for user : {username}</h1>
+    const { data, isLoading, isError, refetch } = useQuery(["cat"], ()=>{
+        return Axios.get("https://catfact.ninja/fact").then((res)=> res.data);
+    } );
+    if(isLoading){
+        return <h1 className="App">Loding...</h1>
+    }
+    if(isError){
+        return <h1 className="App">URL Broken!!</h1>
+    }
+    return (
+    <div className="App">
+        <h1> This is home page for user : {username}</h1> <p>CAT FACT: {data?.fact}</p>
+        <button onClick={refetch}>Get Another Fact!</button>
+    </div>
+    );
 }
