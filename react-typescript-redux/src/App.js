30af8492f2d5bd49dbc4e13966b54e369bddcad1
Siddhant J <sidbnjadhav@gmail.com>
2023-10-06 23:51:59 +0530

    push props-types in react

--- react-typescript-redux/src/App.js
+++ react-typescript-redux/src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import { Person } from './components/Person';
 
 function App() {
   return (
     <div className="App">
+      <Person 
+      name="Siddhant J"
+      email= "sidbnjadhav@gmail.com"
+      age={21}
+      isMarried = {false}
+      friends = {["Vaish","Sahil","Siddharth"]}
+      />
     </div>
   );
 }
