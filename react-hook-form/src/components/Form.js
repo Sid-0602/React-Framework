df32b143dabfd610045714de04f3a533d71d3571
Siddhant J <sidbnjadhav@gmail.com>
2023-10-06 11:15:15 +0530

    Parsing Data with react-hook-form

--- react-hook-form/src/components/Form.js
+++ react-hook-form/src/components/Form.js
@@ -1,13 +1,23 @@
 import '../App.css'
 import {useForm} from "react-hook-form"
+import * as yup from 'yup';
 export const Form = () =>{
+
+    const { register , handleSubmit } = useForm(); //creating a useForm hook to parse form info.
+
+
+    const onSubmit = (data) =>{
+        console.log(data);
+        console.log("Form data submitted!");
+    }
+
     return (
-        <form className='App form'>
-            <input type="text" placeholder="Your Name..."/> <br/> <br/>
-            <input type="text" placeholder="Email..."/> <br/> <br/>
-            <input type="text" placeholder="Age..."/><br/> <br/>
-            <input type="password" placeholder="Enter Passkey.."/><br/> <br/>
-            <input type="password" placeholder="Confirm Passkey.."/><br/> <br/>
+        <form className='App form' onSubmit={handleSubmit(onSubmit)}>
+            <input type="text" placeholder="Your Name..." {...register("fullName")}/> <br/> <br/>
+            <input type="text" placeholder="Email..."{...register("email")}/> <br/> <br/>
+            <input type="number" placeholder="Age..."{...register("age")}/><br/> <br/>
+            <input type="password" placeholder="Enter Passkey.."{...register("password")}/><br/> <br/>
+            <input type="password" placeholder="Confirm Passkey.."{...register("confirmPassword")}/><br/> <br/>
             <input type="submit"/>
         </form>
     )
