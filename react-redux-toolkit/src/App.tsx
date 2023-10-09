3189a7fbfcf59237f5cded2b0f4a6e6bf39d5af4
Siddhant J <sidbnjadhav@gmail.com>
2023-10-09 22:37:42 +0530

    update push

--- react-redux-toolkit/src/App.tsx
+++ react-redux-toolkit/src/App.tsx
@@ -5,18 +5,21 @@ import {Home} from "./pages/Home"
 import { Login } from './pages/Login';
 import { Contact } from './pages/Contact';
 import { Provider } from 'react-redux';
+import { store } from './store';
 
 function App() {
   return (
-  <div>
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/login' element={<Login />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='*' element={<h1>404! Page not found</h1>} />
-      </Routes>
-    </Router>
+  <div className='App'>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/login' element={<Login />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='*' element={<h1>404! Page not found</h1>} />
+        </Routes>
+      </Router>
+    </Provider>
   </div>
   )
 }
